72afd8049fe5aa6304052213956d27c84a5816e1
Awais Ali <48981710+awais-malik716@users.noreply.github.com>
2019-06-24 13:07:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function switchMobile(){
+    document.getElementById('screen').style.display = 'block';
+    document.getElementById('screen-apps-main').style.display = 'none';
+}
+
+function swipeApps(){
+    document.getElementById('screen-apps-main').style.display = 'block';
+    document.getElementById('screen').style.display = 'none';
+}
